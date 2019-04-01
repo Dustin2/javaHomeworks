467ffecd87314e977758fd6b36c0711d50e520d6
DUSTINEITOR <38537077+Dustin2@users.noreply.github.com>
2019-04-01 12:01:36 -0600

    Add files via upload

---
+++ Soft-Tech/src/soft/tech/Empleado.java
@@ -0,0 +1,72 @@
+        /*
+         * To change this license header, choose License Headers in Project Properties.
+         * To change this template file, choose Tools | Templates
+         * and open the template in the editor.
+         */
+        package soft.tech;
+
+       
+        public class Empleado {   
+            
+            public  String nom;
+            public  String cedula;
+            public  int edad;
+            public  boolean casado;     
+            public  double salario;      
+
+        Empleado(){
+        }
+        Empleado(String nom,String cedula,int edad,boolean casado,double salario){
+
+        }
+        public String getnombre(){
+        return nom;
+        }
+        public String getcedula(){
+        return cedula;
+        }
+        public int getedad(){
+        return edad;
+        }
+        public boolean getcasado(){
+        return casado;
+        }
+        public double getsalario(){
+        return salario;
+        }
+        public void setnom(String no){
+        this.nom=no;
+        }
+        public void setcedula(String ce){
+        this.cedula=ce;
+        }
+        public void setedad(int ed){
+            this.edad=ed;
+        }
+        public void setcasado(boolean casa){
+        this.casado=casa;
+        }
+        public void setsalario(double sa){
+        this.salario=sa;
+        }
+        
+        public String experiencia (){
+        String tipotrabajador = null;
+            if (edad<=21) {
+                tipotrabajador="principiante";
+                return tipotrabajador;
+            }
+            if (edad>=22&& edad<=35) {
+                tipotrabajador="intermedio";
+                return tipotrabajador;
+            }
+            if (edad>=35) {
+                tipotrabajador="senior";
+                return tipotrabajador;
+            }
+           
+        return null;
+        }
+   
+}
+        
---
+++ Soft-Tech/src/soft/tech/Programador.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soft.tech;
+
+/**
+ *
+ * @author PROGRAMAR
+ */
+public class Programador extends Empleado{
+    private int lineascodhora;
+    private String lenguajedom;
+    
+    Programador(){
+    }
+    Programador(String name,String cedula,int edad,boolean casado,double sueldo,String lenguajedom,int lineascod){
+        super(name,cedula,edad,casado,sueldo);
+        this.lenguajedom=lenguajedom;
+        this.lineascodhora=lineascod;
+    }
+    
+    public String  getlenguaje(){
+    return lenguajedom;
+    }
+    public int gethoras(){
+    return lineascodhora;
+    }
+    public getlengu(){
+    
+    }
+    
+}
